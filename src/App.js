bff576e75397f0e92c86d81aef8911af5390bd22
Allan Jr. Berse <122271911+Adrianne2407@users.noreply.github.com>
2023-06-06 16:02:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chatapp-s9v5.onrender.com");
+const socket = io.connect("https://chatapp-gmad.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
